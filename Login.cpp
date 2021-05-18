05e1095ba1a8f9918bebb121acedb23f02d3ae78
Kaz Matsuo <matsuo1@stolaf.edu>
2021-05-17 22:06:51 -0500

    Final

--- Login.cpp
+++ Login.cpp
@@ -5,30 +5,31 @@
         followed = 0;
         follow = 0;
             
-        //cerr << "This constructor ran" << endl; 
+        //cerr << "This constructor ran" << endl; testing purposes
         }
         
-    void User_info::Test_for_Lab(){
+    void User_info::Test_for_Lab(){//used for testing in the lab to see if methods run according to call.
             cerr << "This ran" << endl;
         }
-    bool User_info::IsloggedIn(){
+    bool User_info::IsloggedIn(){// a bool function that allows the user to compare the username and password combination to the txt file database 
+    //standard cin inputs for terminal use.
     cerr << "Enter username: ";
     cin >> username;
     cerr << "Enter password: ";
     cin >> password;
 
-    ifstream read(username +".txt");
+    ifstream read(username +".txt");//reading from a file
     getline(read,un);
     getline(read,pw);
 
-    if (un == username && pw ==password){
+    if (un == username && pw ==password){//checking to see if the password matches the username 
         return true;
     }
     else{
         return false;
     }
     }
-    int User_info::login_test(){
+    int User_info::login_test(){//used to see if the login functionality works in the terminal
     
         cerr << "1: Register"<<endl;
         cerr << "2: Login"<<endl;
@@ -67,13 +68,13 @@
         }
     }
     void User_info::write_to_file(){
-        ofstream f;
-            f.open(username +".txt");
-            f << username << endl;
-            f << password << endl;
-            f << "Email: " << email << endl;
-            f << "Followers: " << followed << endl;
-            f << "Following: " << follow << endl;
+        ofstream f;//opens file connection
+            f.open(username +".txt");//opens a txt file with the username that was created.
+            f << username << endl;//username goes in
+            f << password << endl;//password goes in
+            f << "Email: " << email << endl;//email goes in
+            f << "Followers: " << followed << endl;//dont use
+            f << "Following: " << follow << endl;//dont use
             f.close();
     }
     void User_info::first_page(){
@@ -148,15 +149,15 @@
         f.close();
     }
 
-void User_info::put_in_global_mem(int offset) {
-  username = _global_mem + offset + 4; 
-  password = _global_mem +offset + 4 + 10;
-  email = _global_mem +offset + 4 + 20;
+void User_info::put_in_global_mem(int offset) {//Important in terms of putting stuff into global mem. Also used in the Itinerary function 
+  username = _global_mem + offset + 4; //username would be put inot global mem at the given offset plus size of gm_size
+  password = _global_mem +offset + 4 + 10;//account for username length
+  email = _global_mem +offset + 4 + 20;//account for password length 
   //_put_raw(offset+4, username.c_str());
   //_put_raw(offset+4+len_user,password.c_str());
-  int len_user = username.length()+1;
+  int len_user = username.length()+1;//length + null byte
   int len_pass = password.length()+1;
-  gm_size = 4 + len_user + len_pass;
+  gm_size = 4 + len_user + len_pass;//size of the user info 
   _put_int(offset, gm_size);
   //testing
   //cerr << username << endl;
@@ -164,22 +165,22 @@ void User_info::put_in_global_mem(int offset) {
   //cerr << gm_size << endl;
 }
 
-void User_info::get_from_global_mem(int offset) {
+void User_info::get_from_global_mem(int offset) {//get the information that we stored into global_mem. Used in the Itinerary class as well. 
   gm_size = _get_int(offset);
   username = _global_mem + offset + 4;
   int len_user = username.length()+1;
   password = _global_mem +offset + 4 + len_user;
   int len_pass = password.length()+1;
-  cerr << "Testing for global mem input";
+  cerr << "Testing for global mem input";//testing
   cerr << username;
   cerr << ": ";
   cerr << password;
   cerr << endl;
 }
 bool User_info::check_file(int offset){
-    username = _global_mem + offset + 4; 
+    username = _global_mem + offset + 4; //same method from get_from_global_mem
     password = _global_mem +offset + 4 + 10;
-    ifstream read(username +".txt");
+    ifstream read(username +".txt");//open the txt file to read. Same method used in the 
     getline(read,un);
     getline(read,pw);
     cerr << un << endl;
@@ -192,9 +193,9 @@ bool User_info::check_file(int offset){
     }
 }
 
-void User_info::get_and_register(int offset){
+void User_info::get_and_register(int offset){//used for the first time register button. 
     gm_size = _get_int(offset);
-    username = _global_mem + offset + 4;
+    username = _global_mem + offset + 4;//modifed version of get_from_global_mem
     int len_user = username.length()+1;
     password = _global_mem +offset + 4 + 10;
     int len_pass = password.length()+1;
@@ -203,7 +204,7 @@ void User_info::get_and_register(int offset){
     write_to_file();
 }
 
-bool User_info::check_and_login(int offset){
+bool User_info::check_and_login(int offset){//isolated part of the login functionality to associate the function to a button. 
     bool status = check_file(offset);
         if (!status){
             cerr << "Incorrect Login information!" << endl;
